b521b99f14cb0df4ed48a68c1eb264a13aaef979
dtsym <dtsym@T450sPWR.mshome.net>
2018-01-10 11:21:28 +0100

    Fix pwm8

--- src/KnightRider/KnightRider.java
+++ src/KnightRider/KnightRider.java
@@ -28,7 +28,7 @@ public class KnightRider extends Task{
 	private TPU_PWM pwm5;
 	private TPU_PWM pwm6;
 	private TPU_PWM pwm7;
-	//private TPU_PWM pwm8;
+	private TPU_PWM pwm8;
 
 	
 	public KnightRider() {

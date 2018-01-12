635e8368843fafeeb8cf64388cbea77bb304111b
pduering <pduering@pcinf030.ntb.ch>
2018-01-12 11:05:08 +0100

    displaytest erweitert noch nicht fertig

--- src/DisplayTest.java
+++ src/DisplayTest.java
@@ -1,8 +1,37 @@
 import ch.ntb.inf.deep.runtime.mpc555.driver.HD44780U;
+import ch.ntb.inf.deep.runtime.ppc32.Task;
 
 public class DisplayTest {
-	//Test made by Dimitri
+	private static final char Helloidiot = 0;
+	//Test made by Pascal
 	
 	private HD44780U DisplayObj;
 	
+	public DisplayTest() {
+		
+		DisplayObj = HD44780U.getInstance();
+		
+		DisplayObj.init(2);
+		
+		DisplayObj.onOff(true, true, true);
+		
+		DisplayObj.setCursor(1,1);
+	}
+	
+	public void clear_Display() {
+		
+		DisplayObj.clearDisplay();
+		
+	}
+	
+	public void action() {
+		
+		DisplayObj.writeChar(Helloidiot);
+		
+		// DisplayObj.writeInt(7, 2);
+		
+	}
+	
+
+
 }

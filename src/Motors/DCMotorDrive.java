b1d579af08f43783e3620b1121be14e9a422d0bc
thoma <thoma@DESKTOP-93PV213.fritz.box>
2018-04-22 14:11:50 +0200

    projekt vorbereitung

---
+++ src/Motors/DCMotorDrive.java
@@ -0,0 +1,77 @@
+package Motors;
+
+import java.io.PrintStream;
+
+import ch.ntb.inf.deep.runtime.mpc555.driver.SCI;
+import ch.ntb.inf.deep.runtime.mpc555.driver.TPU_PWM;
+
+public class DCMotorDrive {
+	private static DCMotorDrive dm;  // Diese Klasse hat eine Instanz von sich selber
+
+	private final boolean useTPUA =true;
+	private final int chnL = 0, chnR = 1;
+	private final static int pwmPeriod = 50000 / TPU_PWM.tpuTimeBase;
+
+	private int currHightimeL, currHightimeR;
+	private TPU_PWM pwmL, pwmR;
+	
+	
+	private DCMotorDrive() {
+	// PWM-Kanaele initialisieren
+		currHightimeL = 0;
+		currHightimeR = 0;
+		pwmL = new TPU_PWM(useTPUA, chnL, pwmPeriod, currHightimeL);
+		pwmR = new TPU_PWM(useTPUA, chnR, pwmPeriod, currHightimeR);
+	}
+	
+	
+	public static void leftFull() {
+		dm.update(pwmPeriod, 0);
+	}
+	
+	public static void leftHalf() {
+		dm.update(pwmPeriod/2, 0);
+	}
+	
+	public static void rightFull() {
+		dm.update(0, pwmPeriod);
+	}
+	
+	public static void rightHalf() {
+		dm.update(0, pwmPeriod/2);
+	}
+	
+	public static void stop() {
+		dm.update(0, 0);
+	}
+	
+	
+	private void update(int hightimeL, int hightimeR) {
+		currHightimeL = hightimeL;
+		currHightimeR = hightimeR;
+		pwmL.update(hightimeL);
+		pwmR.update(hightimeR);
+		
+		System.out.print(hightimeL); System.out.print("\t/\t");
+		System.out.print(hightimeR); System.out.print("\t/\t");
+		System.out.print(pwmPeriod); System.out.println();
+	}
+	
+	
+	static {
+		// 1) Initialize SCI1 (9600 8N1)
+		SCI sci1 = SCI.getInstance(SCI.pSCI1);
+		sci1.start(9600, SCI.NO_PARITY, (short)8);
+		
+		// 2) Use SCI1 for stdout
+		System.out = new PrintStream(sci1.out);
+		
+		// Objekt erzeugen
+		dm = new DCMotorDrive();
+		
+		// Kopfzeile Ausgeben
+		System.out.println("Hightime Left \t/\t Hightime Right \t/\t Period");
+	}
+	
+
+}
